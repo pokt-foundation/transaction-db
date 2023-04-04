d80ae6cce6f176387b8d60304c9e048dc347178f
Pablo Ocampo <38480731+pablocampogo@users.noreply.github.com>
2023-04-03 20:46:24 -0500

    feat: add support for ssh tunnel in postgredriver (#10)

--- postgres-driver/postgresdriver.go
+++ postgres-driver/postgresdriver.go
@@ -2,10 +2,31 @@ package postgresdriver
 
 import (
 	"database/sql"
+	"database/sql/driver"
+	"net"
+	"time"
 
 	// PQ import is required
+	"github.com/lib/pq"
 	_ "github.com/lib/pq"
+	"golang.org/x/crypto/ssh"
 )
+
+type ViaSSHDialer struct {
+	client *ssh.Client
+}
+
+func (self *ViaSSHDialer) Open(s string) (_ driver.Conn, err error) {
+	return pq.DialOpen(self, s)
+}
+
+func (self *ViaSSHDialer) Dial(network, address string) (net.Conn, error) {
+	return self.client.Dial(network, address)
+}
+
+func (self *ViaSSHDialer) DialTimeout(network, address string, timeout time.Duration) (net.Conn, error) {
+	return self.client.Dial(network, address)
+}
 
 // The PostgresDriver struct satisfies the Driver interface which defines all database driver methods
 type PostgresDriver struct {
@@ -36,6 +57,24 @@ func NewPostgresDriverFromDBInstance(db *sql.DB) *PostgresDriver {
 	}
 
 	return driver
+}
+
+/* NewPostgresDriver returns PostgresDriver instance from Postgres connection string */
+func NewPostgresDriverWithSSH(connectionString string, sshcon *ssh.Client) (*PostgresDriver, error) {
+	// Now we register the ViaSSHDialer with the ssh connection as a parameter
+	sql.Register("postgres+ssh", &ViaSSHDialer{sshcon})
+
+	db, err := sql.Open("postgres+ssh", connectionString)
+	if err != nil {
+		return nil, err
+	}
+
+	driver := &PostgresDriver{
+		Queries: New(db),
+		db:      db,
+	}
+
+	return driver, nil
 }
 
 func newSQLNullInt32(value int32) sql.NullInt32 {
